24bb6a4b5f99166d5181a7266614b3d360196317
Flavinzan <163418609+flavinzan@users.noreply.github.com>
2024-03-15 10:06:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://s2-autoesporte.glbimg.com/bEz2Mo5Yf43MnjcrHh5BolVDwOc=/0x0:1169x778/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2023/P/W/bjT2Z0RtytJiSE0jNCBQ/porsche-911-gt3-rs-11428-1.jpg"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src = "https://quatrorodas.abril.com.br/wp-content/uploads/2023/08/edit-Porsche-911-GT3-RS-11517_1.jpg?w=1200&h=900"
+}
